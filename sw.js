d493a077ef9d8b783403c2b1b1502f8cf47705ca
lechisy2008 <lechisy2008@gmail.com>
2026-09-20 12:09:44 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,1 @@
-const CACHE='civics-quest-v1';
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest']))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='civics-quest-v4';self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
